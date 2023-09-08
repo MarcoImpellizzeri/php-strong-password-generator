c8a505d9530ecc26058174e818e864a317ae9a82
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-09-08 15:25:17 +0200

    milestone 1 done

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+if (isset($_GET['length'])) {
+    $passwordLength = intval($_GET['length']);
+    $generatedPassword = generatePassword($passwordLength);
+}
+
+function generatePassword($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = rand(0, strlen($characters) - 1);
+        $password .= $characters[$randomIndex];
+    }
+    return $password;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,13 +29,19 @@
 </head>
 
 <body>
-    <div class="container">
-        <form action="index.php">
-            <label>Lunghezza della password:</label>
-            <input type="text">
+    <div class="container p-5 text-center">
+        <h1 class="mb-5">Generatore di Password</h1>
+        <form action="index.php" method="GET">
+            <label class="fw-bold">Inserisci la lunghezza della password: </label>
+            <input type="number" name="length" min="1" max="100" required>
+            <button type="submit" class="btn btn-primary">Genera Password</button>
         </form>
-    </div>
 
+        <?php if (isset($_GET['length'])) { ?>
+        <p class="my-3 fw-bold">la tua password generata è:<span class="text-success"><?php echo " " . $generatedPassword; ?></span></p>
+        <?php } ?>
+    </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 
